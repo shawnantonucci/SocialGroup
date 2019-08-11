6b8a492c098ed535166a31ed124d0f3b329c5405
shawn <shawnantonucci2018@gmail.com>
2019-08-10 19:55:48 -0700

    boilerplate and react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from './components/Navbar'
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import { Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+          <Navbar />
+            <Route path="/" exact component={Login} />
+            <Route path="/dashboard" exact component={Dashboard} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Login</li>
+                <li>Signout</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
